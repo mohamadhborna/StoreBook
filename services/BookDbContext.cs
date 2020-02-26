d2c1afd54e90db096cb35fd5d7834e4bb1ef3eb2
mhborna <bornaamir8@gmail.com>
2020-02-26 19:14:01 +0330

    Solve udemyproject.csproj problems

--- services/BookDbContext.cs
+++ services/BookDbContext.cs
@@ -8,7 +8,6 @@ namespace udemyproject.services
         public virtual DbSet<Auther> Authers{ get; set; }
         public virtual DbSet<Country> Countries{ get; set; }
         public virtual DbSet<Review> Reviews{ get; set; }
-
         public virtual DbSet<Reviewer> Reviewers{ get; set; }
         public virtual DbSet<Category> Categories{ get; set; }
         public virtual DbSet<BookAuther> BookAuthers{ get; set; }
@@ -18,7 +17,18 @@ namespace udemyproject.services
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-           base.OnModelCreating(modelBuilder);
+            //handle many to many ralations
+            modelBuilder.Entity<BookCategory>().HasKey(bc => new{bc.BookId,bc.CategoryId});
+            modelBuilder.Entity<BookCategory>().HasOne(bc =>bc.Book).WithMany(bc => bc.BookCategories).HasForeignKey(bc => bc.BookId);
+            modelBuilder.Entity<BookCategory>().HasOne(bc => bc.Category).WithMany(bc => bc.BookCategories).HasForeignKey(bc => bc.CategoryId);
+            modelBuilder.Entity<BookAuther>().HasKey(ba =>new {ba.BookId,ba.AutherId});
+            modelBuilder.Entity<BookAuther>().HasOne(ba => ba.Book).WithMany(ba =>ba.BookAuthers).HasForeignKey(ba => ba.BookId);
+            modelBuilder.Entity<BookAuther>().HasOne(ba => ba.Auther).WithMany(ba => ba.BookAuthers).HasForeignKey(ba => ba.AutherId);
+            
+            base.OnModelCreating(modelBuilder);
+
+
+           
         }        
     }
 }
